70c2f29b45188bfaf63ab9d7d287eeaf84f7ecf7
Tatiana <www.t97@mail.ru>
2022-11-30 22:59:35 +0300

    задача по семинару 2 (без комментариев)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,6 @@ import static java.lang.Math.random;
 
 public class Main {
     public static void main(String[] args) {
-        /*ArrayList<BaseHero> characterList = new ArrayList<>();
-        characterList.add(new Monk(BaseHero.heroID));
-        characterList.add(new Peasant(BaseHero.heroID));
-        characterList.add(new Robber(BaseHero.heroID));
-        characterList.add(new Sniper(BaseHero.heroID));
-        characterList.add(new Wizard(BaseHero.heroID));
-        characterList.add(new Pikeman(BaseHero.heroID));
-        characterList.add(new Crossbowman(BaseHero.heroID));
-
-        characterList.forEach(n -> System.out.println(n));  // или characterList.forEach(System.out::println);*/
 
         ArrayList<BaseHero> darkTeam = new ArrayList<BaseHero>();
         ArrayList<BaseHero> lightTeam = new ArrayList<BaseHero>();
@@ -40,53 +30,6 @@ public class Main {
             }
             BaseHero.heroID += 1;
         }
-
-
-        /*lightTeam.addMonk(1, "lightTeam");
-        lightTeam.addCrossbowman(3, "lightTeam");
-        lightTeam.addPikeman(2, "lightTeam");
-        lightTeam.addRobber(2, "lightTeam");
-        lightTeam.addPeasant(2, "lightTeam");
-
-
-        darkTeam.addWizard(1, "darkTeam");
-        darkTeam.addSniper(3, "darkTeam");
-        darkTeam.addRobber(3, "darkTeam");
-        darkTeam.addPeasant(3, "darkTeam");
-
-        for (BaseHero hero: lightTeam) {
-            System.out.println(hero);
-        }*/
-
-        /*for (int i = 0; i < 10; i++) {
-            int random = (int)(random()*4);
-            switch (random){
-                case 0:
-                    lightTeam.add(new Monk(BaseHero.heroID, "lightTeam"));
-                case 1:
-                    lightTeam.add(new Peasant(BaseHero.heroID, "lightTeam"));
-                case 2:
-                    lightTeam.add(new Robber(BaseHero.heroID, "lightTeam"));
-                default:
-                    lightTeam.add(new Sniper(BaseHero.heroID, "lightTeam"));
-            }
-            BaseHero.heroID += 1;
-        }
-
-        for (int i = 0; i < 10; i++) {
-            int random = (int)(random()*3);
-            switch (random){
-                case 0:
-                    darkTeam.add(new Peasant(BaseHero.heroID, "darkTeam"));
-                case 1:
-                    darkTeam.add(new Pikeman(BaseHero.heroID, "darkTeam"));
-                case 2:
-                    darkTeam.add(new Wizard(BaseHero.heroID, "darkTeam"));
-                default:
-                    darkTeam.add(new Crossbowman(BaseHero.heroID, "darkTeam"));
-            }
-            BaseHero.heroID += 1;
-        }*/
 
         for (BaseHero hero: darkTeam) {
             System.out.println(hero.getInfo());
--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -5,73 +5,46 @@ public class Team {
     public int teamSize;
     public ArrayList<BaseHero> team = new ArrayList<BaseHero>();
 
-    //protected static int teamID = 1;
-    /*public Team(int teamSize){
-        this.teamSize = teamSize;
-        ArrayList<BaseHero> team = new ArrayList<>(teamSize);
-        Random rand = new Random();
-        /*for (int i = 0; i < teamSize; i++) {
-            int charSelection = rand.nextInt(0, 7);
-            switch (charSelection) {
-                case 0 -> team.add(new Peasant());
-                case 1 -> team.add(new Monk());
-                case 2 -> team.add(new Crossbowman());
-                case 3 -> team.add(new Pikeman());
-                case 4 -> team.add(new Robber());
-                case 5 -> team.add(new Sniper());
-                case 6 -> team.add(new Wizard());
-            }
-        }*/
-
-    public void getHero(int heroIndex) {
-    }
-    /*public void addPeasant(int peasantAmount, String teamName){
+    public void addPeasant(int peasantAmount, String teamName){
         for (int i = 0; i < peasantAmount; i++) {
             BaseHero.heroID += 1;
             team.add(new Peasant(BaseHero.heroID, teamName));
         }
-    }*/
-
+    }
     public void addPeasant(String teamName) {
         BaseHero.heroID += 1;
         team.add(new Peasant(BaseHero.heroID, teamName));
     }
-
     public void addCrossbowman(int crossbowmanAmount, String teamName) {
         for (int i = 0; i < crossbowmanAmount; i++) {
             BaseHero.heroID += 1;
             team.add(new Crossbowman(BaseHero.heroID, teamName));
         }
     }
-
     public void addMonk(int monkAmount, String teamName) {
         for (int i = 0; i < monkAmount; i++) {
             BaseHero.heroID += 1;
             team.add(new Monk(BaseHero.heroID, teamName));
         }
     }
-
     public void addPikeman(int pikemanAmount, String teamName) {
         for (int i = 0; i < pikemanAmount; i++) {
             BaseHero.heroID += 1;
             team.add(new Pikeman(BaseHero.heroID, teamName));
         }
     }
-
     public void addRobber(int robberAmount, String teamName) {
         for (int i = 0; i < robberAmount; i++) {
             BaseHero.heroID += 1;
             team.add(new Robber(BaseHero.heroID, teamName));
         }
     }
-
     public void addSniper(int sniperAmount, String teamName) {
         for (int i = 0; i < sniperAmount; i++) {
             BaseHero.heroID += 1;
             team.add(new Sniper(BaseHero.heroID, teamName));
         }
     }
-
     public void addWizard(int wizardAmount, String teamName) {
         for (int i = 0; i < wizardAmount; i++) {
             BaseHero.heroID += 1;
